9e37bbcdc5bcbbd0535311c1c3f64c16bea33563
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-07-14 23:13:55 +0530

    Update app.module.ts
    added Mat modules
    
    Co-Authored-By: Dharmin Shah <33176750+dharminshah75@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
+import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +20,9 @@ import { from } from 'rxjs';
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]

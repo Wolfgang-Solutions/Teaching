2d31f36d3cea9a5a707ce02bc713c839eba93b2a
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-07-14 21:55:59 +0530

    Check for content

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-post-create',
-    templateUrl: './post-create.component.html';
+    templateUrl: './post-create.component.html'
 })
 export class PostCreateComponent {
-
+    NewPost = 'NO CONTENT';
+    onAddPost() {
+        alert('Post Added');
+        this.NewPost = 'User\'s post';
+    }
 }

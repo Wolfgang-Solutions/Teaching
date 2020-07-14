5eaa677fbb368f2bb8891e278a0af423d1e5d28d
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-07-14 21:15:30 +0530

    Update post-create.component.ts

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-post-create',
+    templateUrl: './post-create.compoment.html'
+})
+export class PostCreateComponent {
+
+}

afa65199f651f03a0207381320d8eec033fb07dc
Dharmin Shah <dharminshah75@gmail.com>
2020-07-14 22:40:06 +0530

    Added pricing and download components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PricingComponent} from './pricing/pricing.component';
+import {DownloadComponent} from './download/download.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pricing' , component: PricingComponent },
+  { path: 'download' , component: DownloadComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { DownloadComponent } from './download/download.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PricingComponent,
+    DownloadComponent
   ],
   imports: [
     BrowserModule,

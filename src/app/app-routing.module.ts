8b11a00ef1135287944e8bcde1bd087944ece9bb
Dharmin Shah <33176750+dharminshah75@users.noreply.github.com>
2020-07-16 22:23:49 +0530

    Dharmin (#7)
    * Added Header component
    
    * Added pricing and download components
    
    * Added angular material
    
    * Added footer
    
    * Updated Footer
    
    * Updated pricing component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PricingComponent} from './pricing/pricing.component';
+import {DownloadComponent} from './download/download.component';
+import {MainComponent} from './main/main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: 'pricing' , component: PricingComponent },
+  { path: 'download' , component: DownloadComponent },
+  { path: '', component: MainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { DownloadComponent } from './download/download.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PricingComponent,
+    DownloadComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

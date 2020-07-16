bb89f843860514579f8a883202729d394e2f024c
Dharmin Shah <dharminshah75@gmail.com>
2020-07-16 18:24:54 +0530

    Updated pricing component

--- src/app/pricing/pricing.component.ts
+++ src/app/pricing/pricing.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-pricing',
   templateUrl: './pricing.component.html',
-  styleUrls: ['./pricing.component.scss']
+  styleUrls: ['./pricing.component.scss']          
 })
 export class PricingComponent implements OnInit {
+
+  public plans = [
+    {"name":"Basic" , "price": 1000},
+    {"name":"Premium" , "price": 2000},
+    {"name":"Pro" , "price": 3000},
+    {"name":"Advanced" , "price": 4000}
+  ];
 
   constructor() { }
 

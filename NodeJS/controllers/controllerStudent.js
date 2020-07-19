f37201aa3d6c1632601a41785dc6397a17aaa0b6
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-07-19 18:36:07 +0530

    Update controllerStudent.js

--- NodeJS/controllers/controllerStudent.js
+++ NodeJS/controllers/controllerStudent.js
@@ -3,12 +3,42 @@ var router = express.Router();
 
 var { Student } = require('../models/student');
 const { json } = require('body-parser');
+const e = require('express');
 router.get('/', (req, res)=>{
     Student.find((err, docs)=>{
         if(!err) { res.send(docs); }
         else { console.log('Error in retreiving student details: ' + JSON.stringify(err, undefined, 2)); }
     })
 });
+
+router.get('/', (req, res)=>{
+    Student.find((err, docs)=>{
+        if(!err) { res.send(docs) }
+        else { console.log('Error in retreiving Students. ' + JSON.stringify(err, undefined, 2)); }
+    })
+})
+
+router.post('/', (req, res)=>{
+    var stud = new Student({
+        name: req.body.name,
+        mid_name :  req.body.mid_name,
+        surname : req.body.surname,
+        grade : req.body.grade,
+        school : req.body.school,
+        board : req.body.name,
+        subscription : req.body.name,
+        phone : req.body.name,
+        email : req.body.name,
+        parent_name: req.body.name,
+        parent_surname : req.body.name,
+        parent_phone: req.body.name,
+        parent_email: req.body.name
+    });
+    stud.save((err, docs) => {
+        if(!err) { res.send(docs); }
+        else { console.log('Error in saving record: ' + JSON.stringify(err, undefined, 2)); }
+    });
+})
 
 module.exports = router;
 

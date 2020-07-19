8707f312aabf1232fff32762f07afbf7e43e79c3
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-07-19 16:39:00 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://derek:trek6000@cluster0.megwb.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const uri = "mongodb+srv://derek:trek6000@cluster0.megwb.mongodb.net/Teach?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
   client.close();
 });
+
+module.exports = MongoClient

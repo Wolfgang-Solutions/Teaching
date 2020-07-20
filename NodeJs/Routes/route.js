43728919f526085bdda9a42755819a2bb08f4c5d
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-07-20 18:23:27 +0530

    Update route.js

--- NodeJs/Routes/route.js
+++ NodeJs/Routes/route.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const Tutor = require('../models/tutors');
-const tutors = require('../models/tutors');
 
 //retrieving tutor contact details
 router.get('/tutors', (req, res, next)=> {
@@ -15,7 +14,8 @@ router.get('/tutors', (req, res, next)=> {
 //adding tutor
 router.post('tutor', (req, res, next)=> {
     //logic to add contact
-    await db.collection('Teacher').insertOne({
+    let newTutor = new Tutor({
+    //await db.collection('Teacher').insertOne({
         name: req.body.name,
         middle_name: req.body.middle_name,
         surname: req.body.surname,
@@ -23,13 +23,26 @@ router.post('tutor', (req, res, next)=> {
         subject: req.body.subject,
         phone: req.body.phone,
         email: req.body.email,
-        //password: req.body.password;
+        //password: req.body.password;   
+    });
+   
+
+    newTutor.save((err, tutor)=>{
+        if(!err)
+        res.json({msg: 'New Tutor added successfully'});
+        else
+        res.json({msg: 'Failed to connect'});
     });
 });
 
 //delete tutor
 router.delete('/tutor/:id', (req, res, next)=>{
-    //delete tutor
+    Tutor.remove({_id: req.params.id}, function(err, result){
+         if(!err)
+         res.json(result);
+         else
+         res.json(err);
+    });
 })
 
 /*router.get('/students', (res, req, next)=>{

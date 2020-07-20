457540b276b3cdcb9345f4542e3315c07e0acc5e
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-07-20 16:24:57 +0530

    Created new stack

--- NodeJS/index.js
+++
@@ -1,9 +0,0 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-
-const { MongoClient } = require('./db');
-
-var app = express();
-app.use(bodyParser.json());
-
-app.listen(3000, () => console.log('Server started at port : 3000'));

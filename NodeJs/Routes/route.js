15b7ec3dab28c785997a1a294f712d38c5b01385
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-07-20 17:37:48 +0530

    Update route.js

--- NodeJs/Routes/route.js
+++ NodeJs/Routes/route.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+//retrieving tutor contacts
 router.get('/tutors', (req, res, next)=> {
     res.send('Retrieving the constact list');
 });
+
+//adding tutor
+router.post('tutor', (req, res, next)=> {
+    //logic to add contact
+});
+
+//delete tutor
+router.delete('/tutor/:id', (req, res, next)=>{
+    //delete tutor
+})
 
 /*router.get('/students', (res, req, next)=>{
     res.send('Retrieving student contact list')

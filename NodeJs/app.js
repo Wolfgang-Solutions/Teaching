eb3894ecdcf22fa41b386bc65f14f8264d718705
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-07-20 16:33:20 +0530

    Create app.js

---
+++ NodeJs/app.js
@@ -0,0 +1,14 @@
+//importing modules
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var path = require('path');
+
+var app = express();
+
+//port no
+const port = 3000;
+app.listen(port,()=>{
+    console.log('Server started at port:'+port);
+});

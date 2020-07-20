3be20d553c674ee091d70c4ab48187cd3f09936b
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-07-20 17:37:42 +0530

    Update tutors.js

--- NodeJs/models/tutors.js
+++ NodeJs/models/tutors.js
@@ -1,1 +1,13 @@
 const mongoose  = require('mongoose');
+
+const contactSchema = mongoose.Schema({
+    name: { type: String, required: true }, 
+    middle_name: { type: String, required: false },
+    surname: { type: String, required: true },
+    subject: { type: String, required: true },
+    email: { type: String, required: true},
+    phone: {type: Int32Array, required: true},
+    bio: { type: String, required: true}
+})
+
+const Contact = module.exports = mongoose.model('Contact', contactSchema);

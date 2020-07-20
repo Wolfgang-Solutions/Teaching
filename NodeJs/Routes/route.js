b4376a694ad29a43083146fab1839d6ad468db64
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-07-20 17:04:55 +0530

    Update route.js

---
+++ NodeJs/Routes/route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/tutors', (req, res, next)=> {
+    res.send('Retrieving the constact list');
+});
+
+router.get('/students', (res, req, next)=>{
+    res.send('Retrieving student contact list')
+});
+
+module.exports = router;

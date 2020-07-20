f4c430b368e9e74fa864ee5acb42e5ae54d0e6ed
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-07-20 16:53:51 +0530

    Update app.js

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -7,8 +7,27 @@ var path = require('path');
 
 var app = express();
 
+const route = require('./Routes/route')
+
 //port no
 const port = 3000;
+
+//middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//routes
+app.use('/api', route);
+
+//testing sever connection 
+app.get('/', (req, res)=>{  //req = Request parameter res = Response Parameter
+    res.send('Running');
+});
+
+
 app.listen(port,()=>{
-    console.log('Server started at port:'+port);
+    console.log('Server started at port:' + port);
 });

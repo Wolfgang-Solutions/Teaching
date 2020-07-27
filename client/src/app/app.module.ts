5e9ad8309d92849fe89fa98f27decfaebbab6172
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-07-27 19:17:38 +0530

    Added Material theme

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PricingComponent } from './pricing/pricing.component';
 import { DownloadComponent } from './download/download.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
 import { SubjectsComponent } from './subjects/subjects.component';
 import { HttpClientModule } from '@angular/common/http';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

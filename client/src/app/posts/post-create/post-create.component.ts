4d728713948beb84dc23dd4245ce985a2b12fe46
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-07-28 16:19:38 +0530

    update create-post component

--- client/src/app/posts/post-create/post-create.component.ts
+++ client/src/app/posts/post-create/post-create.component.ts
@@ -5,5 +5,10 @@ import { Component } from '@angular/core';
     templateUrl: './post-create.component.html'
 })
 export class  PostCreateComponent {
+    NewPost = '';
 
+    onAddPost() {
+        this.NewPost = 'The user\'s post';
+       // alert('post added!');
+    }
 }

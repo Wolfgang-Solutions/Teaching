31fbab9232ae29e8712be876728542bd2a53d88f
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-07-28 16:33:40 +0530

    Update app.module.ts
    Please keep your component imports seperate from library/framework imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { PricingComponent } from './pricing/pricing.component';
-import { DownloadComponent } from './download/download.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { FooterComponent } from './footer/footer.component';
-import { MainComponent } from './main/main.component';
-import { SubjectsComponent } from './subjects/subjects.component';
 import { HttpClientModule } from '@angular/common/http';
 // import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
+import { SubjectsComponent } from './subjects/subjects.component';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DownloadComponent } from './download/download.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PricingComponent } from './pricing/pricing.component';
+
 
 @NgModule({
   declarations: [

c5871e8b8ef9e1fce1d703266eb06c12cfdd2c04
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-07-29 14:27:09 +0530

    Update app.component.ts

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Post } from './posts/post.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Teaching';
-
-  posts = [];
+  storedPosts: Post[] = [];
 
   onPostAdded(post){
-    this.posts.push(post);
+    this.storedPosts.push(post);
   }
 }

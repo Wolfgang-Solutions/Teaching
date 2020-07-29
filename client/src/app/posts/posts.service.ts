2737769dcb45fe9582386f46fe32c921057f5257
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-07-29 16:05:02 +0530

    Update posts.service.ts

--- client/src/app/posts/posts.service.ts
+++ client/src/app/posts/posts.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 import { Post } from './post.model';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
     private posts: Post[] = [];
+    private postsUpdated = new Subject<Post[]>();
 
     getPosts() {
         return [...this.posts]; // to copy elements from old array
     }
 
+    getPostUpdateListener() {
+        return this.postsUpdated.asObservable();
+    }
+
     addPosts(title: string, content: string) {
         const post: Post = {title: title, content: content};
         this.posts.push(post);
+        this.postsUpdated.next([...this.posts]); // copy of posts array
     }
 }
